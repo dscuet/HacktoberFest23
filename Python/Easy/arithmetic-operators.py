1f845298a47e349a3ed5a6e9fe38aa9b61f5130a
Your Name <you@example.com>
2023-10-21 03:32:32 +0500

    arithmetic-operators

---
+++ Python/Easy/arithmetic-operators.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Read the two integers from STDIN
+
+
+# Calculate the required values
+sum_ab = a + b
+diff_ab = a - b
+product_ab = a * b
+
+# Print the results
+print(sum_ab)
+print(diff_ab)
+print(product_ab)
