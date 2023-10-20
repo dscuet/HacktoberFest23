4ddde067e9277fecb7456575b85cb85007be4fe6
Abdul Sabur <alsabur20@gmail.com>
2023-10-20 07:25:21 +0000

    Solved averyBigSum

---
+++ Python/Easy/aVeryBigSum.py
@@ -0,0 +1,9 @@
+
+#Author: Abdul Sabur
+#URL: https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+def aVeryBigSum(ar):
+    # Write your code here
+    sum=0
+    for num in ar:
+        sum+=num
+    return sum
