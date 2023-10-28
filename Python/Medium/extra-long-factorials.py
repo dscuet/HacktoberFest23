8e3617a7f90e28b7742d4196f8f93cdec880a8b9
Ahmad Aziz <ahamadaziz7@gmail.com>
2023-10-28 14:48:32 +0000

    add solution description

--- Python/Medium/extra-long-factorials.py
+++ Python/Medium/extra-long-factorials.py
@@ -1,6 +1,15 @@
 # Ahmad Aziz
 # https://www.hackerrank.com/challenges/closest-number/problem
 
+"""
+    Solution:
+    
+    1) Find the value of a^b
+    2) Divide that value by X (will result in a float most of the times)
+    3) Round/Floor that divided number to get an integer
+    4) Multiply that integer by X to get the closest multiple of X to a^b 
+""" 
+
 def closestNumber(a, b, x):
     """
     Calculate the closest number to a^b which is a multiple of x.
